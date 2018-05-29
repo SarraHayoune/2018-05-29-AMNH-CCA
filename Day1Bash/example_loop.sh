9df6a7ab017b5d531c5249c49b5a5e2cc39acff9
AstroCom <astrocom@Davids-MacBook-Pro.local>
2018-05-29 15:22:28 -0400

    deleted everythin except the  loop

--- Day1Bash/example_loop.sh
+++ Day1Bash/example_loop.sh
@@ -1,13 +1,13 @@
 
 
 #!/bin/bash
-# I am creating a variable greeting
-greeting='Hello World'
-# I am printing the contents of greeting
-echo $greeting
-#touch and print a range of files in a loop
+
+
+
+
+
 for myfile in {A..Z}.txt;
 do
     touch $myfile
     echo $myfile
-done
+done 
