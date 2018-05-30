13bb9751209f9ba7653884caba2b8cbaca17a4ce
AstroCom <astrocom@Davids-MacBook-Pro.local>
2018-05-30 15:22:44 -0400

    function_to_use

---
+++ Day2Python/functions_to_use.py
@@ -0,0 +1,8 @@
+
+'''this module contains some functions we will create'''
+def division_function_to_test(dividend, divisor):
+    dividend=dividend*1.0
+    result= dividend/divisor
+    return result
+# exampleattribute
+example_dividend= 300
